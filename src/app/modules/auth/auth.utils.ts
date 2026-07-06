86fc73713f388552ab907d94f300ca0b00c86106
MdMuzahid07 <mdmuzahid7396@gmail.com>
2026-07-06 14:07:16 +0600

    fix: build issue

--- src/app/modules/auth/auth.utils.ts
+++ src/app/modules/auth/auth.utils.ts
@@ -9,7 +9,7 @@ export const createToken = (
   expiresIn: string
 ) => {
   return jwt.sign(jwtPayload, secret, {
-    expiresIn,
+    expiresIn: expiresIn as jwt.SignOptions['expiresIn'],
   });
 };
 
